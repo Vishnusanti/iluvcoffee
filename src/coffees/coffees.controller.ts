fc606d4e9f970aba0c7b7437664f04f3f2535829
Vishnusanti <94221623+Vishnusanti@users.noreply.github.com>
2021-11-13 15:47:44 +0530

    initial commit
    initial commit of nestjs learning

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService){}
+    @Get()
+    findAll(@Query() paginationQuery)
+    {
+        //const{limit, offset} = paginationQuery;
+
+        return this.coffeesService.findAll();
+        //return `This just return all the data of coffees ${limit}, ${offset}`;
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string)
+    {
+        return this.coffeesService.findOne(id);
+        //return `This action returns #${id} coffee`;
+    }
+
+    @Post()
+    create(@Body() createCoffeesDto: CreateCoffeeDto)
+    {
+        return this.coffeesService.create(createCoffeesDto);
+        //return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto)
+    {
+        return this.coffeesService.update(id, updateCoffeeDto);
+        //return `This action will update the data of #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string)
+    {
+        return this.coffeesService.remove(id);
+        //return `This action will delete the #${id} coffee`;
+    }
+}
